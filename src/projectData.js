fd1e2e9a7dc187f4eaecfe37530432f17ef009a9
Christopher <cvariara@gmail.com>
2024-08-26 17:55:49 -0400

    Added Data Analyst Projects

--- src/projectData.js
+++ src/projectData.js
@@ -2,7 +2,14 @@ export const fsdProjects = [
   {
     id: 1,
     title: "WeConnect",
-    tech_stack: ["MongoDB", "Express", "Node.js", "React.js", "Socket.io", "Material UI"],
+    tech_stack: [
+      "MongoDB",
+      "Express",
+      "Node.js",
+      "React.js",
+      "Socket.io",
+      "Material UI",
+    ],
     description:
       "WeConnect is a dynamic messaging web application built using the MERN stack. It offers users the ability to create personalized profiles and connect with friends through messaging. Upon registering, users can set up a profile with details such as name and profile picture. They can then search for and add friends within the platform. WeConnect facilitates real-time messaging between friends, enabling users to engage in private conversations. The app aims to provide a seamless and user-friendly experience for individuals looking to connect and communicate with their network.",
     github: "https://github.com/cvariara/we-connect",
@@ -22,7 +29,13 @@ export const fsdProjects = [
   {
     id: 3,
     title: "E-Commerce Site",
-    tech_stack: ["JavaScript", "React", "Tailwind CSS", "Fake Store API", "React Router DOM"],
+    tech_stack: [
+      "JavaScript",
+      "React",
+      "Tailwind CSS",
+      "Fake Store API",
+      "React Router DOM",
+    ],
     description:
       "This website uses JavaScript, React, and Tailwind CSS to design and implement a dynamic e-commerce website. Utilized the React Router DOM for seamless navigation, ensuring a smooth and intuitive user experience. Integrates the Fake Store API allowed to populate the website with realistic product data.",
     github: "https://github.com/cvariara/shopping-cart",
@@ -49,28 +62,72 @@ export const fsdProjects = [
     demo: "https://cvariara.github.io/weather-app/",
     img: "/projects/weather-app.jpg",
   },
-  {
-    id: 6,
-    title: "Battleship",
-    tech_stack: ["HTML", "CSS", "JavaScript", "npm", "Jest"],
-    description:
-      "Place your ships on your board. When you're done, the game starts. Take turns with the computer to attack the opponent's board. First one to eliminate all 5 of the opponent's ships wins. Uses Jest to test the code.",
-    github: "https://github.com/cvariara/battleship",
-    demo: "https://cvariara.github.io/battleship/",
-    img: "/projects/battleship.png",
-  },
+  // {
+  //   id: 6,
+  //   title: "Battleship",
+  //   tech_stack: ["HTML", "CSS", "JavaScript", "npm", "Jest"],
+  //   description:
+  //     "Place your ships on your board. When you're done, the game starts. Take turns with the computer to attack the opponent's board. First one to eliminate all 5 of the opponent's ships wins. Uses Jest to test the code.",
+  //   github: "https://github.com/cvariara/battleship",
+  //   demo: "https://cvariara.github.io/battleship/",
+  //   img: "/projects/battleship.png",
+  // },
 ];
-
 
 export const daProjects = [
   {
-    id: 11,
-    title: "AirBnB Dashboard",
+    id: 4,
+    title: "Exploratory Data Analysis of Company Layoffs",
+    tech_stack: ["SQL"],
+    description:
+      "Conducted exploratory data analysis using SQL on a dataset of company layoffs from 2020 to 2023. The project involved examining trends, identifying key factors influencing layoffs, and uncovering patterns over the years, providing insights into the economic and industry-specific impacts of layoffs during this period.",
+    github:
+      "https://github.com/cvariara/DataAnalystProjects/blob/main/Exploratory%20Data%20Analysis%20Project.sql",
+    // demo: "https://docs.google.com/spreadsheets/d/1MSuXUwXWKKtCI50XhSRAHnpTNgmAaG4H/edit?usp=sharing&ouid=110944581781816126219&rtpof=true&sd=true",
+    img: "",
+  },
+  {
+    id: 5,
+    title: "Exploratory Data Analysis of World Population",
+    tech_stack: ["Python", "Pandas", "Seaborn", "Matplotlib"],
+    description:
+      "Conducted an exploratory data analysis using Python and Pandas on a dataset of world population by country from 1970 to 2022. The project reveals key trends, growth patterns, and demographic changes across different regions and periods.",
+    github:
+      "https://github.com/cvariara/DataAnalystProjects/blob/main/Pandas%20-%20Exploratory%20Data%20Analysis.ipynb",
+    // demo: "https://docs.google.com/spreadsheets/d/1MSuXUwXWKKtCI50XhSRAHnpTNgmAaG4H/edit?usp=sharing&ouid=110944581781816126219&rtpof=true&sd=true",
+    img: "",
+  },
+  {
+    id: 1,
+    title: "Airbnb Dashboard",
     tech_stack: ["Tableau"],
-    description: "Developed an Airbnb dashboard using the Seattle Airbnb Open Data set for the 2016 calendar year. The dashboard provides insights into prices, the number of available Airbnbs, bedroom counts, and income generated in the area, offering a comprehensive view of the Airbnb market in Seattle.",
+    description:
+      "Developed an Airbnb dashboard using the Seattle Airbnb Open Data set for the 2016 calendar year. The dashboard provides insights into prices, the number of available Airbnbs, bedroom counts, and income generated in the area, offering a comprehensive view of the Airbnb market in Seattle.",
     // github: "",
     demo: "https://public.tableau.com/views/AirbnbDashboardProject_17221750113990/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
     img: "/projects/airbnb-dashboard-tableau.png",
+  },
+  {
+    id: 2,
+    title: "Bike Sales Dashboard",
+    tech_stack: ["Excel"],
+    description:
+      "Developed a dashboard that visualizes bike sales data from Kaggle. After thorough data cleaning, pivot tables were employed to generate insightful charts. The dashboard features interactive filters for marital status, region, and education, providing a dynamic analysis experience.",
+    github:
+      "https://github.com/cvariara/DataAnalystProjects/blob/main/Excel%20Project%20Dataset.xlsx",
+    // demo: "https://docs.google.com/spreadsheets/d/1MSuXUwXWKKtCI50XhSRAHnpTNgmAaG4H/edit?usp=sharing&ouid=110944581781816126219&rtpof=true&sd=true",
+    img: "/projects/bikesales-dashboard-excel.png",
+  },
+  {
+    id: 3,
+    title: "Professional Survey Dashboard",
+    tech_stack: ["Power BI"],
+    description:
+      "Developed a dashboard using Power BI, leveraging a dataset from a survey of professionals. After thorough data cleaning, the dashboard showcases key insights with interactive graphs and charts, highlighting survey takers' countries, average salaries, demographics, challenges in breaking into data analytics, and overall happiness levels.",
+    github:
+      "https://github.com/cvariara/DataAnalystProjects/blob/main/Data%20Survey%20Project.pbix",
+    // demo: "https://docs.google.com/spreadsheets/d/1MSuXUwXWKKtCI50XhSRAHnpTNgmAaG4H/edit?usp=sharing&ouid=110944581781816126219&rtpof=true&sd=true",
+    img: "/projects/data-professional-survey-dashboard-powerbi.png",
   },
 ];
 
@@ -78,75 +135,74 @@ export const daTech = [
   {
     id: 1,
     language: "Python",
-    img: "/languages/python.png"
+    img: "/languages/python.png",
   },
   {
     id: 2,
     language: "SQL",
-    img: "/languages/sql.png"
+    img: "/languages/sql.png",
   },
   {
     id: 3,
     language: "Tableau",
-    img: "/languages/tableau.png"
+    img: "/languages/tableau.png",
   },
   {
     id: 4,
     language: "Excel",
-    img: "/languages/excel.png"
+    img: "/languages/excel.png",
   },
   {
     id: 5,
     language: "PowerBI",
-    img: "/languages/powerbi.png"
-  }
+    img: "/languages/powerbi.png",
+  },
 ];
-
 
 export const fsdTech = [
   {
     id: 1,
     language: "React",
-    img: "/languages/react.png"
+    img: "/languages/react.png",
   },
   {
     id: 2,
     language: "Express.js",
-    img: "/languages/express.png"
+    img: "/languages/express.png",
   },
   {
     id: 3,
     language: "JavaScript",
-    img: "/languages/javascript.png"
+    img: "/languages/javascript.png",
   },
   {
     id: 4,
     language: "HTML",
-    img: "/languages/html5.png"
+    img: "/languages/html5.png",
   },
   {
     id: 5,
     language: "CSS",
-    img: "/languages/css3.png"
+    img: "/languages/css3.png",
   },
   {
     id: 6,
     language: "Python",
-    img: "/languages/python.png"
+    img: "/languages/python.png",
   },
   {
     id: 7,
     language: "Java",
-    img: "/languages/java.png"
+    img: "/languages/java.png",
   },
   {
     id: 8,
     language: "C",
-    img: "/languages/c.png"
+    img: "/languages/c.png",
   },
   {
     id: 9,
     language: "SQL",
-    img: "/languages/sql.png"
-  }
+    img: "/languages/sql.png",
+  },
 ];
